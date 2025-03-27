5bcad2b692e393e3aecc6cb76d369192f3ed1b13
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-03-26 20:44:54 -0600

    create the todolist on react native

---
+++ components/GoalInput.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Button, TextInput, View, StyleSheet } from "react-native";
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccccc",
+    width: "70%",
+    marginRight: 8,
+    height: "20%",
+    paddingInline: 10,
+  },
+});
+
+export default GoalInput;
